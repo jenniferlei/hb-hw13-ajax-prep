d1c508f03718d78ccbc909805ac5886f32f0479d
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-30 02:01:38 +0000

    Added AJAX to get profiles

--- profile.py
+++ profile.py
@@ -17,10 +17,27 @@ def profile():
     """Return results from profile form."""
 
     fullname = request.json['name']
+    age = request.json['age']
+    occupation = request.json['occupation']
+    salary = request.json['salary']
+    education = request.json['education']
+    state = request.json['state']
+    city = request.json['city']
+    garden = request.json['garden']
+    tv = request.json['tv']
+
     # TODO: get the values from the rest of the form
     # Add them to jsonify
     
-    return jsonify({'fullname': fullname, })
+    return jsonify({'fullname': fullname,
+                    'age': age,
+                    'occupation': occupation,
+                    'salary': salary,
+                    'education': education,
+                    'state': state,
+                    'city': city,
+                    'garden': garden,
+                    'tv': tv,})
 
 
 
--- static/profile.js
+++ static/profile.js
@@ -3,9 +3,30 @@ function submitProfile(evt) {
 
   const data = {
     name: document.querySelector('#name-field').value,
-    // fill in the rest
+    age: document.querySelector('#age-field').value,
+    occupation: document.querySelector('#occupation-field').value,
+    salary: document.querySelector('[name="salary"]').value,
+    education: document.querySelector('[name="education"]').value,
+    state: document.querySelector('#state-field').value,
+    city: document.querySelector('[name="city"]').value,
+    garden: document.querySelector('[name="garden"]').value,
+    tv: document.querySelector('[name="tv"]').value,
   };
 
+  fetch('/api/profile', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(responseJson => {
+      document.querySelector('#profiles').insertAdjacentHTML('beforeend',
+      `<li>${responseJson.fullname} is ${responseJson.age} and works as a ${responseJson.occupation}
+      <br>They have a salary of ${responseJson.salary}, with an education level of ${responseJson.education}
+      <br>They live in a(n) ${responseJson.city} part of ${responseJson.state}</li>`);
+    });
   // make request to server to get the data
   // add the data the server returns to the document
   // (you can add it to the end of the div with ID "profiles")
